dfda124a64ee182d0687bc498d5d3409373155a9
David Johansson <david.johansson4@tre.se>
2021-03-07 11:12:49 +0100

    Add first test

---
+++ scripts/aws-invoke.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pushd ../
+ aws lambda invoke --invocation-type RequestResponse --function-name fplperiod --region eu-north-1 --log-type Tail --payload fileb://test.json outfile.json && cat outfile.json | jq
+ popd
--- test/fivepelo_aggr/core_test.clj
+++ test/fivepelo_aggr/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [fivepelo-aggr.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest groupByWeekAndPerson-test
+  (def sut (groupByWeekAndPerson (testData)))
+  (def firstWeekParticipants (comp :participants #(first %)))
+  (def firstWeekParticipantsFirstTraining (comp #(map first %) #(map :training %) #(firstWeekParticipants %)))
+
+  (testing "A list of length 1"
+    (is (= 2 (count sut)))
+    (is (= 48 (get (first sut) :weekOfYear)))
+    (is (= '("David Johansson" "Johanna Ljung") (map :name (firstWeekParticipants sut))))
+    (is (= '("Löpning Rocklunda" "Yoga") (map :activity (firstWeekParticipantsFirstTraining sut))))
+    (is (= '("20201201" "20201131") (map :date (firstWeekParticipantsFirstTraining sut))))
+
+    (is (= 49 (get (nth sut 1) :weekOfYear)))
+
+    )
+  )
